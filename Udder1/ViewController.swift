6d00a6d17368024b972c3c7d6810823dd4b483f1
Yunus OZDEMIR <imlm.yunus@gmail.com>
2019-07-02 14:14:24 +0300

    SD

---
+++ Udder1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder1
+//
+//  Created by macOS on 2.07.2019.
+//  Copyright © 2019 macOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
